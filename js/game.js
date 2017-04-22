ef9c9a8202092a312adbc365f03a0465cc3a94af
Hunter Loftis <hunter@hunterloftis.com>
2017-04-22 05:27:09 -0400

    consuming bodies

--- js/game.js
+++ js/game.js
@@ -2,7 +2,10 @@ function Game(input) {
   const snake = Snake(-500, 0)
   const planet = Body(150)
   const moon = Body(15, 200, 10, planet)
-  const galaxy = Galaxy(planet, moon)
+  const moon2 = Body(20, 400, 30, planet)
+  const asteroid = Body(5, 50, 5, moon2)
+  const asteroid2 = Body(3, 100, 6, moon2)
+  const bodies = [ planet, moon, moon2, asteroid, asteroid2 ]
 
   return {
     update,
@@ -10,14 +13,14 @@ function Game(input) {
   }
 
   function update(seconds) {
-    galaxy.update(seconds)
-    snake.update(seconds, input)
+    bodies.forEach(body => body.update(seconds))
+    snake.update(seconds, input, bodies)
   }
 
   function getState() {
     return {
       snake: snake.getState(),
-      bodies: galaxy.getState()
+      bodies: bodies.map(body => body.getState())
     }
   }
 }
@@ -37,7 +40,13 @@ function Snake(x, y) {
     getState
   }
 
-  function update(seconds, input) {
+  function update(seconds, input, bodies) {
+    move(seconds, input)
+    const collisions = bodies.filter(isColliding)
+    collisions.forEach(eat)
+  }
+
+  function move(seconds, input) {
     if (input.left) state.clockwise = false
     else if (input.right) state.clockwise = true
     const sign = state.clockwise ? 1 : -1
@@ -52,6 +61,21 @@ function Snake(x, y) {
     state.position.splice(100)
   }
 
+  function isColliding(body) {
+    if (body.isConsumed) return false
+    const bodyState = body.getState()
+    const range = bodyState.size + state.size
+    const dx = bodyState.x - state.position[0].x
+    const dy = bodyState.y - state.position[0].y
+    const dist = Math.sqrt(dx * dx + dy * dy)
+    return dist < range
+  }
+
+  function eat(body) {
+    if (body.isLargerThan(state.size)) return
+    state.size += body.consume()
+  }
+
   function getState() {
     return Object.assign({
       x: state.position[0].x,
@@ -60,19 +84,13 @@ function Snake(x, y) {
   }
 }
 
-function Galaxy(...bodies) {
-  return {
-    update(seconds) { bodies.forEach(body => body.update(seconds)) },
-    getState() { return bodies.map(body => body.getState()) }
-  }
-}
-
 function Body(size, orbit=0, period=1, parent) {
-  const speed = Math.PI * 2 / period
   let angle = 0
-  return {
+  const speed = Math.PI * 2 / period
+  const instance = {
     update(seconds) {
       angle += speed * seconds
+      return instance
     },
     getState() {
       const origin = parent ? parent.getState() : { x: 0, y: 0 }
@@ -81,6 +99,18 @@ function Body(size, orbit=0, period=1, parent) {
         y: origin.y + Math.sin(angle) * orbit,
         size: size
       }
+    },
+    consume() {
+      let consumed = size
+      size = 0
+      return consumed
+    },
+    get isConsumed() {
+      return size === 0
+    },
+    isLargerThan(otherSize) {
+      return size >= otherSize
     }
   }
+  return instance
 }
